dd9475ecaa70d04439363ae3423304a32180f1fc
Charles Sampson <csampson@qualtrics.com>
2022-11-30 11:44:05 -0700

    setup template

---
+++ 2022/1.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const path = process.argv[1].split('/');
+const day = path[path.length-1].split('.')[0];
+
+const fn = `${__dirname}/input/${day}.txt`;
+
+function a() {
+    const input = fs.readFileSync(fn, 'utf-8');
+    console.log(`day${day} part a`);
+    console.log(input);
+}
+
+function b() {
+    const input = fs.readFileSync(fn, 'utf-8');
+    console.log(`day${day} part b`);
+    console.log(input);
+}
+
+
+
+
+const funcs = {
+    a,
+    b
+};
+const func = process.argv[2];
+if (func) {
+    if (func === 'a' || func === 'b') {
+        funcs[func]();
+    } else {
+        console.error(`invalid arg: '${func}' expected 'a' or 'b'`);
+    }
+} else {
+    console.error('please specify if you want to run part a or b');
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent
+
+Stuff I wrote for [advent of code](https://adventofcode.com/)
