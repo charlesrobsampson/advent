001b75f41fbe71f91f08efab8ab6360e8cd7ba2b
Charles <csampson@westech-inc.com>
2022-12-01 23:00:28 -0700

    day 2 solution

--- 2022/2.js
+++ 2022/2.js
@@ -1,10 +1,62 @@
 function a(input) {
-    console.log(input);
+    const win = {
+        X: 'C',// rock -> scissors
+        Y: 'A',// paper -> rock
+        Z: 'B'// scissors -> paper
+    };
+    const tie = {
+        X: 'A',// rock -> rock
+        Y: 'B',// paper -> paper
+        Z: 'C'// scissors -> scissors
+    };
+    const vals = {
+        X: 1,
+        Y: 2,
+        Z: 3
+    };
+    const rounds = input.split('\r\n');
+    let score = 0;
+    rounds.forEach(round => {
+        const [elf, you] = round.split(' ');
+        score += vals[you];
+        if (elf === win[you]) {
+            score += 6;
+        } else if (elf === tie[you]) {
+            score += 3;
+        }
+    });
+    console.log(`score: ${score}`);
 }
 
 function b(input) {
-    console.log(input);
-}
+    const lose = {
+        A: 'C',// rock -> scissors
+        B: 'A',// paper -> rock
+        C: 'B'// scissors -> paper
+    };
+    const win = {
+        C: 'A',// scissors -> rock
+        A: 'B',// rock -> paper
+        B: 'C'// paper -> scissors
+    };
+    const vals = {
+        A: 1,
+        B: 2,
+        C: 3
+    };
+    const rounds = input.split('\r\n');
+    let score = 0;
+    rounds.forEach(round => {
+        const [elf, outcome] = round.split(' ');
+        if (outcome === 'X') {
+            score += vals[lose[elf]];
+        } else if (outcome === 'Y') {
+            score += vals[elf] + 3;
+        } else {
+            score += vals[win[elf]] + 6;
+        }
+    });
+    console.log(`score: ${score}`);}
 
 module.exports = {
     a,
